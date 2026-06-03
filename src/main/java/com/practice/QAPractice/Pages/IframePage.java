1e2f6da0ba6f2199e2072479c7842829ddc63818
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-03 12:50:59 +0300

    Create IframePage.java

---
+++ src/main/java/com/practice/QAPractice/Pages/IframePage.java
@@ -0,0 +1,13 @@
+package com.practice.QAPractice.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.practice.QAPractice.BasePage.BasePage;
+
+public class IframePage extends BasePage {
+
+	public IframePage(WebDriver driver) {
+		super(driver);
+	}
+
+}
