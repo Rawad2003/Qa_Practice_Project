e3f3472af74bf1507ab9774c5b94a21300d30343
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-03 12:27:55 +0300

    Declared the URLs

--- src/main/java/com/practice/QAPractice/Pages/DragAndDropPage.java
+++ src/main/java/com/practice/QAPractice/Pages/DragAndDropPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import com.practice.QAPractice.BasePage.BasePage;
 
 public class DragAndDropPage extends BasePage {
+	private static final String URL_BOXES = "https://www.qa-practice.com/elements/dragndrop/boxes";
+	private static final String URL_IMAGES = "https://www.qa-practice.com/elements/dragndrop/images";
 
 	public DragAndDropPage(WebDriver driver) {
 		super(driver);
