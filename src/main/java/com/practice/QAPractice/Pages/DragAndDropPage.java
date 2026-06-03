5c3829d62212e076b07de3536b48bacb01ec9921
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-03 12:35:35 +0300

    declared the wanted elements

--- src/main/java/com/practice/QAPractice/Pages/DragAndDropPage.java
+++ src/main/java/com/practice/QAPractice/Pages/DragAndDropPage.java
@@ -1,5 +1,6 @@
 package com.practice.QAPractice.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.practice.QAPractice.BasePage.BasePage;
@@ -7,6 +8,10 @@ import com.practice.QAPractice.BasePage.BasePage;
 public class DragAndDropPage extends BasePage {
 	private static final String URL_BOXES = "https://www.qa-practice.com/elements/dragndrop/boxes";
 	private static final String URL_IMAGES = "https://www.qa-practice.com/elements/dragndrop/images";
+
+	private final By dropAbleBox = By.cssSelector("#rect-droppable");
+	private final By dragAbleBox = By.cssSelector("#rect-draggable");
+	private final By dropAbleText = By.cssSelector("#text-droppable");
 
 	public DragAndDropPage(WebDriver driver) {
 		super(driver);
