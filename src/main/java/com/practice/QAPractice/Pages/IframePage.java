8b9a53c921e4170788f8b6450f8036be5d28daa0
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-03 12:52:14 +0300

    initialized the URL

--- src/main/java/com/practice/QAPractice/Pages/IframePage.java
+++ src/main/java/com/practice/QAPractice/Pages/IframePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import com.practice.QAPractice.BasePage.BasePage;
 
 public class IframePage extends BasePage {
+	private static final String IFRAMES_URL = "https://www.qa-practice.com/elements/iframe/iframe_page";
 
 	public IframePage(WebDriver driver) {
 		super(driver);
