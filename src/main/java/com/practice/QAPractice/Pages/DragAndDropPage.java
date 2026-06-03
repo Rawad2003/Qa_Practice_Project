e4a4c46906cc88353b31949f670844907807df12
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-03 12:26:26 +0300

    Create DragAndDropPage.java

---
+++ src/main/java/com/practice/QAPractice/Pages/DragAndDropPage.java
@@ -0,0 +1,13 @@
+package com.practice.QAPractice.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.practice.QAPractice.BasePage.BasePage;
+
+public class DragAndDropPage extends BasePage {
+
+	public DragAndDropPage(WebDriver driver) {
+		super(driver);
+	}
+
+}
