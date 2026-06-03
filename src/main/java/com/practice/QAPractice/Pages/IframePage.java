3e2e4d199214f8fcfe60fd47bef1dccd38695748
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-03 12:55:28 +0300

    initalized the iframes

--- src/main/java/com/practice/QAPractice/Pages/IframePage.java
+++ src/main/java/com/practice/QAPractice/Pages/IframePage.java
@@ -1,11 +1,15 @@
 package com.practice.QAPractice.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.practice.QAPractice.BasePage.BasePage;
 
 public class IframePage extends BasePage {
 	private static final String IFRAMES_URL = "https://www.qa-practice.com/elements/iframe/iframe_page";
+
+	private final By iframe = By.cssSelector("iframe[class='embed-responsive-item']");
+	private final By anyIframe = By.tagName("iframe");
 
 	public IframePage(WebDriver driver) {
 		super(driver);
