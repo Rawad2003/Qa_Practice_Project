03efc13aeeb81bbf7df6ebab26b8511602091830
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-04 13:26:36 +0300

    initialized the Iframe Variables and elements

--- src/main/java/com/practice/QAPractice/Pages/IframePage.java
+++ src/main/java/com/practice/QAPractice/Pages/IframePage.java
@@ -10,6 +10,21 @@ public class IframePage extends BasePage {
 
 	private final By iframe = By.cssSelector("iframe[class='embed-responsive-item']");
 	private final By anyIframe = By.tagName("iframe");
+	private final By navbarToggler = By.cssSelector(".navbar-toggler");
+	private final By navbarMenu = By.id("navbarHeader");
+	private final By aboutParagraph = By.cssSelector("#navbarHeader h4 + p, #navbarHeader p.text-muted");
+	private final By twitterLink = By.linkText("Follow on Twitter");
+	private final By facebookLink = By.linkText("Like on Facebook");
+	private final By emailLink = By.linkText("Email me");
+	private final By albumBrand = By.cssSelector("a.navbar-brand");
+	private final By mainCallToActionButton = By.linkText("Main call to action");
+	private final By secondaryActionButton = By.linkText("Secondary action");
+	private final By firstViewButton = By.xpath("(//button[normalize-space()='View'])[1]");
+	private final By firstEditButton = By.xpath("(//button[normalize-space()='Edit'])[1]");
+	private final By backToTopLink = By.linkText("Back to top");
+	private final By visitTheHomePageLink = By.linkText("Visit the homepage");
+	private final By gettingStartedGuideLink = By.linkText("getting started guide");
+	private final By footer = By.cssSelector("footer");
 
 	public IframePage(WebDriver driver) {
 		super(driver);
