195decf2df9956654b5d0c804fd2e9a8f5997e3b
Rawad2003 <rawad_isayed@yahoo.com>
2026-06-04 15:59:37 +0300

    modifications and initialization
    modified the variables and initialized the Launch button and first method of Modal Methods

--- src/main/java/com/practice/QAPractice/Pages/PopUpPage.java
+++ src/main/java/com/practice/QAPractice/Pages/PopUpPage.java
@@ -1,7 +1,11 @@
 package com.practice.QAPractice.Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.practice.QAPractice.BasePage.BasePage;
 
@@ -10,14 +14,17 @@ public class PopUpPage extends BasePage {
 	private static final String URL_MODAL = "https://www.qa-practice.com/elements/popup/modal";
 	private static final String IFRAME_POPUP = "https://www.qa-practice.com/elements/popup/iframe_popup";
 
+	// Shared Elements:
 	private final By launchButton = By.cssSelector("button[data-bs-target='#exampleModal']");
 	private final By modal = By.id("exampleModal");
 	private final By modalShown = By.cssSelector(".modal.show");
 	private final By modalTitle = By.id("exampleModalLabel");
-	private final By checkBox = By.id("id_checkbox_0");
 	private final By closeButton = By.cssSelector(".modal-footer .btn-secondary");
 	private final By sendButton = By.cssSelector(".modal-footer .btn-primary");
+	// Modal Elements:
+	private final By checkBox = By.id("id_checkbox_0");
 	private final By resultSection = By.xpath("//p[contains(text(),'Selected checkboxes')]");
+	// Iframe Pop-Up Elements:
 	private final By textCopy = By.id("text-to-copy");
 	private final By pasteTextInput = By.xpath("//input[contains(@class,'textinput')]");
 	private final By popupIframe = By.cssSelector("#exampleModal iframe");
@@ -34,5 +41,29 @@ public class PopUpPage extends BasePage {
 
 	public void navigateToIframePopup() {
 		driver.get(IFRAME_POPUP);
+	}
+
+	// Launch Pop-Up Button Methods:
+	public boolean isLaunchButtonVisible() {
+		return driver.findElement(launchButton).isDisplayed();
+	}
+
+	public boolean isLaunchButtonEnabled() {
+		return driver.findElement(launchButton).isEnabled();
+	}
+
+	public void clickLaunchButton() {
+		driver.findElement(launchButton).click();
+	}
+
+	// Modal Methods:
+	public boolean isModalVisible() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(modalShown));
+			return driver.findElement(modal).isDisplayed();
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
